3070388b5e8aab3811b4f7b2c91098cb3d20bfda
shane424 <detroitdude04@gmail.com>
2015-10-21 15:49:23 -0400

    comments

--- Register/UCReg.py
+++ Register/UCReg.py
@@ -6,42 +6,36 @@ import time
 import username as un 
 import password as pw
 import searchNumber as sN
-# from selenium.webdriver.common.actions
 searchNumber = sN.searchNumber
+
+# Opens firefox
 driver = webdriver.Firefox()
 driver.get('https://ixpress-server2.uc.edu/Registration/Register.asp')
+# Finds username/password text boxes
 user = driver.find_element_by_name("Username")
 passw = driver.find_element_by_name("Password")
 user.send_keys(un.username)
 passw.send_keys(pw.password)
+# Clicks log in
 log = driver.find_element_by_xpath("//input[@value='Log In']").click()
-# log.send_keys("Keys.RETURN")
-# log.submit()
-# time.sleep(5)
+# Hits register
 regbut = driver.find_element_by_xpath("//input[@value='Register']").click()
+# Finds drop down
 termcode = driver.find_element_by_name("TermCode")
 selection = Select(termcode.find_element_by_xpath("//select[@name='TermCode']"))
+# Clicks semester
 selection.select_by_visible_text('Spring 2015-16')
 conbut = driver.find_element_by_xpath("//input[@value='Continue']").click()
-# time.sleep(5)
+# Writes number into first box 
 numb1 = driver.find_element_by_xpath("//input[@name='CallNumber6']")
 numb1.send_keys(searchNumber)
 sub = driver.find_element_by_xpath("//input[@value='Submit']").click()
-# for i in driver.find_element_by_xpath("//input[@name='CallNumber6']"):
-	# print i.get_attribute('value')
+
 new = driver.find_element_by_xpath("//input[@name='CallNumber6']")
+# Whatever is in the text box after hitting submit. (if worked, nothing. If didn't work, same number)
 theone = new.get_attribute('value')
 while(theone == searchNumber):
-# if(new.get_attribute('value') == '604278'):
 		time.sleep(15)
 		submi = driver.find_element_by_xpath("//input[@value='Submit']").click()
 		new = driver.find_element_by_xpath("//input[@name='CallNumber6']")
 		theone = new.get_attribute('value')
-
-	
-	
-# if(numb1.get_attribute('value') == '604278'):
-	# print "yo"
-# while(driver.find_ele)
-# numb1.submit()
-# send_keys("Keys.ARROW_DOWN")
